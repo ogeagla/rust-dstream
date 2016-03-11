cb5b8bb6a4e75031793c388bde50b101b08a1d29
Octavian Geagla <ogeagla@gmail.com>
2016-03-11 09:33:28 -0700

    working out some primitives

--- src/dstream/dstream2d/mod.rs
+++ src/dstream/dstream2d/mod.rs
@@ -2,9 +2,39 @@ use na::*;
 use std::num::*;
 mod test;
 
-pub struct Grid {
+pub trait GridSpace {
+    fn update_grid_pt_density(&mut self, val_to_add: (f64, f64), current_time: u32) -> Result<(), String>;
+
+    fn compute_decay_factor_at_time(&self, t_n: u32, t_l: u32) -> f64 {
+        LAMBDA.powf((t_n - t_l) as f64)
+    }
+}
+
+pub struct GridPoint {
     i: usize,
     j: usize,
+    density: f64,
+    last_density: f64,
+    new_data_time: u32,
+    last_data_time: u32,
+
+}
+
+impl GridSpace for GridPoint {
+
+    //TODO
+    fn update_grid_pt_density(&mut self, val_to_add: (f64, f64), current_time: u32) -> Result<(), String> {
+        let coeff = self.compute_decay_factor_at_time(self.new_data_time, self.last_data_time);
+        let new_density = coeff * self.last_density + 1.0;
+
+        self.last_density = self.density;
+        self.density = new_density;
+
+        self.last_data_time = self.new_data_time;
+        self.new_data_time = current_time;
+
+        Ok(())
+    }
 }
 
 const C_M: f64     = 3.0;
@@ -12,11 +42,8 @@ const C_L: f64     = 0.8;
 const LAMBDA: f64  = 0.998;
 const BETA: f64    = 0.3;
 
-fn compute_density_coeff_at_time(timestamp: u32, current_time: u32) -> f64 {
-    LAMBDA.powf((current_time - timestamp) as f64)
-}
 
-pub fn initialize_clustering(grid_list: Mat2<Grid>) -> Result<(), String> {
+pub fn initialize_clustering(grid_list: Mat2<GridPoint>) -> Result<(), String> {
     //update density of all grids in grid_list
     //assign each dense grid to a distinct cluster
     //label all other grids as NO_CLASS; bad grids!
@@ -38,13 +65,16 @@ pub fn initialize_clustering(grid_list: Mat2<Grid>) -> Result<(), String> {
 fn initialize_clusters() {
     const loc_i: usize = 0 as usize;
     const loc_j: usize = 0 as usize;
-    const G1: Grid = Grid {i: loc_i, j: loc_j};
+    const G1: GridPoint = GridPoint
+                {i: loc_i, j: loc_j,
+                    density: 0.0, new_data_time: 0, last_data_time: 0,
+                    last_density: 0.0};
     let result = initialize_clustering(Mat2::new(G1, G1, G1, G1));
     assert_eq!(Ok(()), result);
 }
 
 
-pub fn adjust_clustering(grid_list: Mat2<Grid>) -> Result<(), String> {
+pub fn adjust_clustering(grid_list: Mat2<GridPoint>) -> Result<(), String> {
     //update the density of all grids in grid_list
     /*
     foreach grid g whose attribute (dense/sparse/transitional) is changed since last call to adjust_clustering()
